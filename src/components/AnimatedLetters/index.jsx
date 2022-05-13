e3a59a06c9dad20895a11678170e71efe8d6eaeb
sashtje <sashtje.lefevre@gmail.com>
2022-05-13 22:27:59 +0300

    feat: add react-spinner

---
+++ src/components/AnimatedLetters/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import "./styles.scss";
+
+const AnimatedLetters = ({ strArray, startDelay, timeBeforeHoverActive }) => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const [delay, setDelay] = useState(startDelay);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+      setDelay(null);
+    }, timeBeforeHoverActive * 1000);
+  }, []);
+
+  return (
+    <>
+      {strArray.map((item, index) => (
+        <span
+          key={index}
+          style={{
+            animationDelay: `${delay ? delay + index * 0.1 : "0"}s`,
+          }}
+          className={item[1] ? `${item[1]} ${letterClass}` : letterClass}
+        >
+          {item[0]}
+        </span>
+      ))}
+    </>
+  );
+};
+
+export default AnimatedLetters;
