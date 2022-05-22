1f822afde32769f28fd61cb6b2388992986bab8b
sashtje <sashtje.lefevre@gmail.com>
2022-05-22 23:54:14 +0300

    feat: move cvs samples to public folder to get rid of the hash in the names of files

--- src/components/ContactControls/index.jsx
+++ src/components/ContactControls/index.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 
 import "./styles.scss";
 
-import CV from "../../assets/cv/CV_Aleksandra (Sashtje).txt";
-
 const ContactControls = () => {
   return (
     <div className="contact-controls">
@@ -16,7 +14,7 @@ const ContactControls = () => {
       </Link>
       <a
         className="contact-controls__btn contact-controls__download-cv"
-        href={CV}
+        href={`${process.env.PUBLIC_URL}/cv/CV Aleksandra Rogova (Sashtje).txt`}
         download={true}
       >
         Download CV
