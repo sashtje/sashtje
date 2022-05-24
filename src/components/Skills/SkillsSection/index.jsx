c70cb6c99389e6c8f6a4df01302ad2b4b17764f6
sashtje <sashtje.lefevre@gmail.com>
2022-05-24 18:13:19 +0300

    feat: add dark styles for skills section

--- src/components/Skills/SkillsSection/index.jsx
+++ src/components/Skills/SkillsSection/index.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import SkillsItem from "../SkillsItem";
 
+import { SettingsContext } from "./../../../context/index";
+
 const SkillsSection = ({ title, skillsArray }) => {
+  let { isLightTheme } = useContext(SettingsContext);
+
   return (
     <section className="skills__section">
       <h2 className="skills__section-title">{title}</h2>
       <div className="section-skills">
-        {skillsArray.map(({ icon, subtitle }, index) => (
-          <SkillsItem key={index} icon={icon} subtitle={subtitle} />
+        {skillsArray.map(({ icon, subtitle, iconDark }, index) => (
+          <SkillsItem
+            key={index}
+            icon={!isLightTheme && iconDark ? iconDark : icon}
+            subtitle={subtitle}
+          />
         ))}
       </div>
     </section>
--- src/model/skills.js
+++ src/model/skills.js
@@ -25,7 +25,11 @@ import { GiOctopus, GiStoneBlock } from "react-icons/gi";
 import { FaGithubAlt } from "react-icons/fa";
 
 const codeSkills = [
-  { icon: <SiMarkdown color="#000000" />, subtitle: "Markdown" },
+  {
+    icon: <SiMarkdown color="#000000" />,
+    subtitle: "Markdown",
+    iconDark: <SiMarkdown color="#dddddd" />,
+  },
   { icon: <SiHtml5 color="#e34f26" />, subtitle: "Html" },
   { icon: <SiCss3 color="#1572b6" />, subtitle: "Css" },
   { icon: <SiSass color="#cc6699" />, subtitle: "Scss" },
@@ -42,7 +46,11 @@ const libsSkills = [
 
 const toolsSkills = [
   { icon: <SiGit color="#f05032" />, subtitle: "Git" },
-  { icon: <SiGithub color="#181717" />, subtitle: "Github" },
+  {
+    icon: <SiGithub color="#181717" />,
+    subtitle: "Github",
+    iconDark: <SiGithub color="#dddddd" />,
+  },
   { icon: <SiWebpack color="#8dd6f9" />, subtitle: "Webpack" },
   { icon: <SiVisualstudiocode color="#007acc" />, subtitle: "Vs Code" },
   { icon: <SiGooglechrome color="#4285f4" />, subtitle: "Dev Tools" },
@@ -58,7 +66,11 @@ const otherSkills = [
   { icon: <GiStoneBlock color="#1769ff" />, subtitle: "Bem" },
   { icon: <SiAdobephotoshop color="#31a8ff" />, subtitle: "Photoshop" },
   { icon: <SiFigma color="#f24e1e" />, subtitle: "Figma" },
-  { icon: <SiInkscape color="#000000" />, subtitle: "Inkscape" },
+  {
+    icon: <SiInkscape color="#000000" />,
+    subtitle: "Inkscape",
+    iconDark: <SiInkscape color="#dddddd" />,
+  },
   { icon: <SiTrello color="#0052cc" />, subtitle: "Trello" },
   { icon: <FaGithubAlt color="#fc6d26" />, subtitle: "GitHub Project" },
 ];
