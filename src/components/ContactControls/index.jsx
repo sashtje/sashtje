71979d70fa40c1705b4e603e60a108fb8f863c82
sashtje <sashtje.lefevre@gmail.com>
2022-05-25 13:47:48 +0300

    feat: translate ContactControls

--- src/components/ContactControls/index.jsx
+++ src/components/ContactControls/index.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import "./styles.scss";
 
+import { SettingsContext } from "../../context";
+
 const ContactControls = () => {
+  let { isEnLang } = useContext(SettingsContext);
+
   return (
     <div className="contact-controls">
       <Link
         className="contact-controls__btn contact-controls__contact-me"
         to="/contacts"
       >
-        Contact me
+        {isEnLang ? "Contact me" : "Написать мне"}
       </Link>
       <a
         className="contact-controls__btn contact-controls__download-cv"
         href={`${process.env.PUBLIC_URL}/cv/CV Aleksandra Rogova (Sashtje).txt`}
         download={true}
       >
-        Download CV
+        {isEnLang ? "Download CV" : "Скачать CV"}
       </a>
     </div>
   );
