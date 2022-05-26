62ddfde99064b5574225c1664919c69cbf4c1317
sashtje <sashtje.lefevre@gmail.com>
2022-05-26 22:06:12 +0300

    refactor: delete unused import

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { RingLoader } from "react-spinners";
 
 import AnimatedLetters from "./../../components/AnimatedLetters/";
