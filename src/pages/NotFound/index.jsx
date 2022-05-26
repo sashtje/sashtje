3eaec874a56192b3e5b4e5269898596c84de9d03
sashtje <sashtje.lefevre@gmail.com>
2022-05-26 19:45:52 +0300

    feat: add content for NotFound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 import "./styles.scss";
 
+import { SettingsContext } from "../../context";
+
 const NotFound = () => {
-  return <div>Page not found</div>;
+  let { isEnLang } = useContext(SettingsContext);
+
+  return (
+    <div className="page__content not-found">
+      <div className="not-found__content">
+        {isEnLang ? (
+          <>
+            Page not found.{" "}
+            <Link className="link" to="/">
+              Go to Home page.
+            </Link>
+          </>
+        ) : (
+          <>
+            Страница не найдена.{" "}
+            <Link className="link" to="/">
+              Перейти на домашнюю страницу.
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
